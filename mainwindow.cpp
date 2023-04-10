335aff1a71513f6dfde5daa8b179f921683d3154
georgiyburmistrov <burmistrov.nsgsm@gmail.com>
2023-04-10 13:54:49 +0400

    First elements were added

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,10 +6,17 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    socket = new QTcpSocket(this);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    socket -> connectToHost("127.0.0.1", 2323);
 }
 
